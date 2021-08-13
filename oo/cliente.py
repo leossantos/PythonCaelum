232a65b74c458db3690cd8028b442156284a57d8
leonardo <leonardo.santos@petlove.com.br>
2021-08-12 22:24:23 -0300

    Terminando OO

--- oo/cliente.py
+++ oo/cliente.py
@@ -1,6 +1,30 @@
 class Cliente:
 
     def __init__(self, nome, sobrenome, cpf):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.cpf = cpf
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+
+    @property
+    def nome(self):
+        return self.nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def sobrenome(self):
+        return self.sobrenome.title()
+
+    @sobrenome.setter
+    def sobrenome(self, sobrenome):
+        self.__sobrenome = sobrenome
+
+    @property
+    def cpf(self):
+        return self.cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
--- oo/conta.py
+++ oo/conta.py
@@ -1,11 +1,11 @@
-from historico import Historico
+from oo.historico import Historico
+
 
 class Conta:
-
     identificador = 0
     __slots__ = ['_numero', '_titular', '_saldo', '_limite', '_historico', '_identificador']
-
-    def	__init__(self, numero, cliente, saldo, limite):
+    # Construtor
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
         self._numero = numero
         self._titular = cliente
         self._saldo = saldo
@@ -14,21 +14,25 @@ class Conta:
         self._identificador = Conta.identificador
         Conta.identificador += 1
 
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
     @property
     def saldo(self):
-        return self._saldo				
+        return self._saldo
 
     @saldo.setter
-    def saldo(self,	saldo):
+    def saldo(self, saldo):
         if (saldo < 0):
             print("saldo não pode ser negativo")
         else:
-            self._saldo	= saldo
+            self._saldo = saldo
 
     @property
     def numero(self):
         return self._numero
-    
+
     @property
     def titular(self):
         return self._titular
@@ -36,7 +40,7 @@ class Conta:
     @property
     def limite(self):
         return self._limite
-    
+
     @limite.setter
     def limite(self, limite):
         if (limite < 0):
@@ -52,24 +56,28 @@ class Conta:
     def id(self):
         return self._identificador
 
-    def	deposita(self, valor):
+    def deposita(self, valor):
         self.historico.transacoes.append("depositou {}".format(valor))
         self.saldo += valor
-   
-    def	saca(self,	valor):
-        if (self.saldo < valor):
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self._saldo + self._limite
+        return valor_a_sacar <= valor_disponivel
+
+    def saca(self, valor):
+        if self.__pode_sacar(valor):
             return False
         else:
             self.saldo -= valor
             self.historico.transacoes.append("saque	de {}".format(valor))
             return True
 
-    def	extrato(self):
+    def extrato(self):
         print("Nome: {} {}".format(self.titular.nome, self.titular.sobrenome))
         print("id: {}   numero: {} \nsaldo: {}".format(self.id, self.numero, self.saldo))
         self.historico.transacoes.append("tirou extrato - saldo de {}".format(self.saldo))
 
-    def	transfere_para(self, destino, valor):
+    def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
         if (retirou == False):
             return False
@@ -77,5 +85,3 @@ class Conta:
             destino.deposita(valor)
             self.historico.transacoes.append("transferencia de {} para conta {}".format(valor, destino.numero))
             return True
-
-
